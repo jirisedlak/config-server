95a60294132e7e48c93f4df7ee9e6a64c023b340
Jiri Sedlak <jirka@sedlaci.net>
2016-09-19 16:17:24 +0200

    initial server

---
+++ src/main/java/net/jirisedlak/spring/cloud/config/server/ConfigServerApplication.java
@@ -0,0 +1,17 @@
+package net.jirisedlak.spring.cloud.config.server;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableConfigServer
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(ConfigServerApplication.class).web(true).run(args);
+    }
+
+}
